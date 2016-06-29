49330b6c0562d4465159476c9111fdc17492fcd6
nealith <nealith@neaworld.fr>
2016-06-29 16:00:21 +0200

    debug, add waterfall view, and some features

--- HarViewer.js
+++ HarViewer.js
@@ -1,5 +1,7 @@
 function HarViewer(){
 
+    var viewer = this;
+
     this.entriesSummaries = new Array();
     this.showedEntries = new Array();
 
@@ -9,7 +11,7 @@ function HarViewer(){
     this.left = null;
     this.right = null;
     this.table = null;
-    this.listFilters = null;
+    this.listFilters = ['Name','URL','Method','Status','Type','Size','Time'];
     this.filters = null;
     this.time = null;
     this.waterfall = null;
@@ -22,6 +24,7 @@ function HarViewer(){
         this.entriesSummaries = new Array();
         this.har = har;
         this.entries = parseHar(har);
+        this.initEntriesSummaries();
 
         this.initSearchBar();
         this.initLeftRight();
@@ -40,6 +43,8 @@ function HarViewer(){
         $('#HarViewer').append(this.form);
         $('#HarViewerForm').append(this.search);
         $('#HarViewerForm').keypress( this.onSearch );
+        $('#HarViewerForm').click( this.onClickOnSearch );
+        $('#HarViewerForm').focusout( this.onFocusOutSearch );
 
     }
 
@@ -73,7 +78,7 @@ function HarViewer(){
             $('#HarViewerTable').remove();
         }
 
-        this.table = document.createElement( 'table' );
+        this.table = document.createElement( 'div' );
         this.table.id = 'HarViewerTable';
 
         $('#HarViewerLeftPanel').append(this.table);
@@ -83,22 +88,24 @@ function HarViewer(){
     }
 
     this.initFilterBar = function(){
-        this.listFilters = ['Name','URL','Method','Status','Type','Size','Time'];
 
-        this.filters = document.createElement( 'tr' );
+
+        this.filters = document.createElement( 'div' );
         this.filters.id = 'HarViewerFilters';
         $('#HarViewerTable').append(this.filters);
 
         for (var i = 0; i < this.listFilters.length; i++) {
 
-            var filter = document.createElement( 'th' );
-            filter.id = this.listFilters[i];
+            var filter = document.createElement( 'div' );
+            filter.id = 'HarViewerFilters'+this.listFilters[i];
             filter.innerHTML = this.listFilters[i];
             $('#HarViewerFilters').append(filter);
+            $('#'+filter.id).addClass(filter.id);
+
 
         }
 
-        this.initEntriesSummaries();
+        this.initEntriesRows();
 
     }
 
@@ -107,6 +114,9 @@ function HarViewer(){
         this.time = 0;
 
         for (var i = 0; i < this.entries.length; i++) {
+
+            // beautify the url to make a name of the resources
+
             var url = this.entries[i].request.url;
 
             var c = 0;
@@ -121,22 +131,39 @@ function HarViewer(){
 
             var name = url.substring(c);
 
+            // beautify the type
+
+            var brutType = this.entries[i].response.content.mimeType;
+            var d = 0;
+            find = false;
+            for (var j = 0; (j < brutType.length && !find); j++) {
+                if(brutType[j] == ';'){
+                    d = j;
+                    find = true;
+                }
+
+            }
+            var type = brutType.substring(0,d);
+            if (!find) {
+                type = brutType;
+            }
+
             entry = {
                 Name: name,
                 URL: this.entries[i].request.url,
                 Method: this.entries[i].request.method,
                 Status: this.entries[i].response.status,
-                Type: this.entries[i].response.content.mimeType,
+                Type: type,
                 Size: this.entries[i].response.content.size,
                 Time: this.entries[i].time,
                 Timings: this.entries[i].timings
             };
-            this.time = this.time + entry.time;
+            this.time = this.time + entry.Time;
             this.showedEntries.push(entry);
             this.entriesSummaries.push(entry);
         }
 
-        this.initEntriesRows();
+
     }
 
     this.initEntriesRows = function(){
@@ -145,24 +172,32 @@ function HarViewer(){
 
             var entry = this.showedEntries[i];
 
-            var row = document.createElement('tr');
+            var row = document.createElement('div');
             row.id = 'HarViewerRow_'+i;
 
             $('#HarViewerTable').append(row);
+            $('#HarViewerRow_'+i).addClass('HarViewerTableTd');
 
             for (var j = 0; j < this.listFilters.length; j++) {
                 this.listFilters[j]
-                var field = document.createElement('td');
+                var field = document.createElement('div');
                 field.id = 'HarViewerRow_'+this.listFilters[j]+'_'+i;
                 if (this.listFilters[j] == 'URL') {
-                    field.innerHTML = '<a href="entry[this.listFilters[j]]">'+entry[this.listFilters[j]]+'</a>';
+                    field.innerHTML = '<a href="'+entry[this.listFilters[j]]+'">'+entry[this.listFilters[j]]+'</a>';
                 } else {
-                    field.click(this.onEntry);
+
                     field.innerHTML = entry[this.listFilters[j]];
+
                 }
 
 
                 $('#HarViewerRow_'+i).append(field);
+
+                if (this.listFilters[j] != 'URL') {
+                    $('#HarViewerRow_'+this.listFilters[j]+'_'+i).click( this.onEntry );
+                }
+
+                $('#'+field.id).addClass('HarViewerFilters'+this.listFilters[j]);
             }
         }
     }
@@ -173,10 +208,10 @@ function HarViewer(){
             $('#HarViewerWaterFall').remove();
         }
 
-        this.waterfall = document.createElement( 'table' );
+        this.waterfall = document.createElement( 'div' );
         this.waterfall.id = 'HarViewerWaterFall';
 
-        $('#HarViewerLeftPanel').append(this.waterfall);
+        $('#HarViewerRightPanel').append(this.waterfall);
 
         this.initWaterFallHeader();
         this.showWaterFall();
@@ -185,26 +220,66 @@ function HarViewer(){
 
     this.initWaterFallHeader = function(){
 
-        this.timesTh = document.createElement('tr');
+        this.timesTh = document.createElement('div');
         this.timesTh.id = 'HarViewerTimeTh';
 
         $('#HarViewerWaterFall').append(this.timesTh);
+        $('#HarViewerTimeTh').height($('#HarViewerRow_0').height());
 
-        var divTime = this.time / 10;
+        var divTime = Math.floor(this.time / 10);
 
         for (var i = 0; i < 10; i++) {
 
-            var timeDiv = document.createElement('th');
+            var timeDiv = document.createElement('div');
             timeDiv.id = 'HarViewerTimeTh_'+i;
             timeDiv.innerHTML = divTime*i+'ms';
             $('#HarViewerTimeTh').append(timeDiv);
-
+            $('#'+timeDiv.id).addClass('HarViewerTimeHeader');
         }
 
         this.initWaterFallRows();
     }
 
     this.initWaterFallRows = function(){
+
+        var beginAt = 0;
+
+        for (var i = 0; i < this.showedEntries.length; i++) {
+
+            var entry = this.showedEntries[i];
+
+
+
+            var row = document.createElement('div');
+            row.id = 'HarViewerWaterFallRow_'+i;
+
+            $('#HarViewerWaterFall').append(row);
+            var width = $('#HarViewerWaterFall').width();
+            var trWidth = 0
+            $('#HarViewerWaterFallRow_'+i).addClass('HarViewerWaterFallRow');
+            $('#HarViewerWaterFallRow_'+i).css('padding-left',beginAt);
+            $('#HarViewerWaterFallRow_'+i).height($('#HarViewerRow_0').height());
+
+            for ( timing in entry.Timings ) {
+
+                value = entry.Timings[timing];
+                var block = document.createElement('div');
+                block.id = 'HarViewerWaterFallRow_'+timing+'_'+i;
+                block.alt = value;
+                $('#HarViewerWaterFallRow_'+i).append(block);
+                var classTiming = timing.substring(0,1).toUpperCase()+timing.substring(1);
+                $('#HarViewerWaterFallRow_'+timing+'_'+i).addClass('HarViewerTiming'+classTiming);
+                $('#HarViewerWaterFallRow_'+timing+'_'+i).addClass('HarViewerTimingBlock');
+                var blockWidth = width * value / this.time;
+                $('#HarViewerWaterFallRow_'+timing+'_'+i).width((blockWidth*0.80));
+                trWidth = trWidth + $('#HarViewerWaterFallRow_'+timing+'_'+i).width();
+
+
+            }
+
+            beginAt = trWidth + beginAt;
+
+        }
 
     }
 
@@ -237,37 +312,61 @@ function HarViewer(){
         $('#HarViewerWaterFall').css( 'display', 'none' );
     }
 
-    this.onSearch = function(event){
-        console.log('lol');
-        var str = $('#HarViewerSearch').attr( 'value' );
-        this.showedEntries = new Array();
+    this.onClickOnSearch = function(event){
+        $('#HarViewerSearch').prop( 'value' , '');
+    }
 
-        for (var i = 0; i < this.entriesSummaries.length; i++) {
-            var entry = this.entriesSummaries[i]
+    this.onFocusOutSearch = function(event){
+        $('#HarViewerSearch').prop( 'value' , 'Search...');
+    }
+
+    this.onSearch = function(event){
+        var str = $('#HarViewerSearch').prop( 'value' );
+        viewer.showedEntries = new Array();
+
+        for (var i = 0; i < viewer.entriesSummaries.length; i++) {
+            var entry = viewer.entriesSummaries[i]
             var find = false;
-            for (var i = 0; (i < this.listFilters.length && !find); i++) {
-                if(JSON.stringify(entry[this.listFilters[i]]).includes(str)){
+            for (var j = 0; (j < viewer.listFilters.length && !find); j++) {
+                if (entry[viewer.listFilters[j]].toString().includes(str)) {
                     find = true;
-                    this.showedEntries.push(entry);
                 }
+            }
+            if (find) {
+                viewer.showedEntries.push(entry);
             }
 
         }
 
-        this.initTable();
-        this.initWaterFallView();
+        viewer.initTable();
+        viewer.initWaterFallView();
 
     }
 
     this.onEntry = function(event){
-        if (this.currentEntryFocused == event.currentTarget) {
-            this.hideDetails();
-            this.showWaterFall();
+        var id = event.currentTarget.id;
+
+        var c = 0;
+        var find = false;
+        for (var j = id.length - 1; (j >= 0 && !find); j--) {
+            if(id[j] == '_'){
+                c = j;
+                find = true;
+            }
+
+        }
+
+        var no = id.substring((c+1));
+        var index = no.valueOf();
+        if (index == viewer.currentEntryFocused) {
+            viewer.hideDetails();
+            viewer.showWaterFall();
+            viewer.currentEntryFocused = null;
         } else {
-            this.currentEntryFocused = event.currentTarget;
-            this.initDetails();
-            this.hideWaterFall();
-            this.showDetails();
+            viewer.currentEntryFocused = index;
+            viewer.initDetails();
+            viewer.hideWaterFall();
+            viewer.showDetails();
         }
     }
 }
